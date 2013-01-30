08779543628286ab2835a59c0b5572497e481057
Emantor <phoenix@emantor.de>
2013-01-30 05:38:27 +0100

    Adjust Volume Multimedia keys to pulse
    Adjustet XMonad to use the multimedia keys to control pulse.

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -129,8 +129,9 @@ main = do
         , focusedBorderColor = "#FF0000"
         } `additionalKeys`
         -- Headphone Volume
-        [ ((0, xF86XK_AudioLowerVolume), spawn "amixer set Master playback 1-")
-        , ((0, xF86XK_AudioRaiseVolume), spawn "amixer set Master playback 1+")
+        [ ((0, xF86XK_AudioLowerVolume), spawn "pactl set-sink-volume 0 -- -5%")
+        , ((0, xF86XK_AudioRaiseVolume), spawn "pactl set-sink-volume 0 +5%")
+        , ((0, xF86XK_AudioMute), spawn "~/bin/pa_toggle.sh")
         -- Brightness
         , ((mod1Mask, xF86XK_AudioLowerVolume), spawn "xbacklight -10")
         , ((mod1Mask, xF86XK_AudioRaiseVolume), spawn "xbacklight +10")

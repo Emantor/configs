f88a29f744029ea1f0c92d3c62d0d64e4a9ecedd
Emantor <phoenix@emantor.de>
2013-02-04 16:09:30 +0100

    Add bibliothekar Host-color

--- zsh/custom/80-prompt.zsh
+++ zsh/custom/80-prompt.zsh
@@ -1,11 +1,16 @@
 # Set path to prompt
 fpath+=( $ZSH/prompt )
+
 # Autoload Promptinit AFTER fpath change
 autoload promptinit; promptinit
+
 # Set host-color for some workstations
 zstyle ':prompt:*:Lu-Tze*'  host-color 046
+zstyle ':prompt:*:Bibliothekar*'  host-color 088
+
 # Hide if its the default user
 [[ $USER == phoenix ]] && zstyle ':prompt:*:ps1' hide-user 1
+
 if (( $+functions[prompt_phoenix_setup] )); then
   prompt phoenix
 else

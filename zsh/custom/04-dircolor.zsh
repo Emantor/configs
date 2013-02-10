4e7ae1d61751aaa6fc6e0ffc7d100b4d45df07f0
Emantor <phoenix@emantor.de>
2013-02-10 11:14:46 +0100

    Add LS_COLORS, adjust confs script

---
+++ zsh/custom/04-dircolor.zsh
@@ -0,0 +1,6 @@
+
+# only available for 256 color terminals
+if (( $(echotc Co) == 256 )); then
+    # load trapd00r's LS_COLORS things
+    eval $( dircolors -b $ZSH/external/LS_COLORS/LS_COLORS )
+fi

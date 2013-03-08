bf3afb5baaed0fedc2c405dba359fab7b9519eea
Emantor <phoenix@emantor.de>
2013-03-08 18:20:14 +0100

    Add del-Button

--- zsh/custom/30-bindkeys.zsh
+++ zsh/custom/30-bindkeys.zsh
@@ -2,8 +2,12 @@
 bindkey -v
 # Remap ESC to jj
 bindkey -M viins 'jj' vi-cmd-mode
-
+# Reverse search
 bindkey '^[[Z' reverse-menu-complete
+# Emacs Bindings
 bindkey '^R' history-incremental-search-backward
 bindkey '^A' vi-beginning-of-line
 bindkey '^E' vi-end-of-line
+# Del-Button
+bindkey    "^[[3~"          delete-char
+bindkey    "^[3;5~"         delete-char
